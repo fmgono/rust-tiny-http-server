e1c7e78f21a8c3fa581f0dd5942e8beacf084c0a
Fathan Margono <fmgonoo@gmail.com>
2025-11-21 18:55:20 +0700

    concurrency and  mutate socket
    1. using tokio spawn to handle concurrencies request.
    2. create a mutable buffer to hold a data from request.
    3. setup for routing functionality

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use std::error::Error;
+use std::usize;
+use tokio::io::AsyncReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:3000").await?;
+    println!("Listening on port 3000...");
     loop {
         let (mut socket, addr) = listener.accept().await?;
         println!("Connection established!");
 
-        socket
-            .write_all(b"HTTP/1.1 200 OK\r\n\r\nHello from Rust!")
-            .await
-            .unwrap();
+        tokio::spawn(async move {
+            let mut buffer = [0; 1024];
+            let read_socket = socket.read(&mut buffer).await;
+            let received_batch = match read_socket {
+                Ok(n) => n,
+                Err(arms) => 0,
+            };
+            println!("{:?}", String::from_utf8_lossy(&buffer[..received_batch]));
+            socket
+                .write_all(b"HTTP/1.1 200 OK\r\nConnection: close\r\n\r\nHello from Rust!")
+                .await
+                .unwrap();
+        });
     }
 
     Ok(())
