84a31eb891eef4501d5a1d1cf6a6073f1629f070
Fathan Margono <fmgonoo@gmail.com>
2025-11-21 14:33:42 +0700

    setup tcp listener
    1. using tokio runtime for async process.
    2. bind tcp listener using TcpListener from tokio.
    3. loop block to let the app running infinitely unless we tell it to destroyed.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+        println!("Connection established!");
+
+        socket
+            .write_all(b"HTTP/1.1 200 OK\r\n\r\nHello from Rust!")
+            .await
+            .unwrap();
+    }
+
+    Ok(())
 }
